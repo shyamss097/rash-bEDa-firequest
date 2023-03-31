ff3e2015bdb65a355d9fb99d870f332513df05e8
Shyam Sundar S <shyamss097@gmail.com>
2023-03-31 20:38:20 +0530

    deletion added

--- app.js
+++ app.js
@@ -5,15 +5,25 @@ function renderWaste(doc){
     let li = document.createElement('li');
     let type = document.createElement('span');
     let location = document.createElement('span');
+    let cross = document.createElement('div');
+
 
     li.setAttribute('data-id', doc.id);
     type.textContent = doc.data().type;
     location.textContent = doc.data().location;
+    cross.textContent = 'x';
+
 
     li.appendChild(type);
     li.appendChild(location);
+    li.appendChild(cross);
 
     wasteList.appendChild(li);
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('waste').doc(id).delete();
+    });
 }
 
 
@@ -35,4 +45,20 @@ form.addEventListener('submit', (e) => {
     });
     form.type.value = '';
     form.location.value = '';
+});
+
+
+
+// real-time listener
+db.collection('waste').orderBy('type').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderWaste(change.doc);
+        } else if (change.type == 'removed'){
+            let li = wasteList.querySelector('[data-id=' + change.doc.id + ']');
+            wasteList.removeChild(li);
+        }
+    });
 });
